8666508ec8d163ef7c4b7000bb0b91935389a214
Angelina Bezik <angelina1337bz@gmail.com>
2024-01-19 20:30:38 +0300

    feat: add solution for 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs'); 
+const path = require("path");
+
+const folderPath = path.join(__dirname, "secret-folder");
+
+fs.readdir(folderPath,
+    { withFileTypes: true },
+    (err, files) => {
+        console.log("\nCurrent directory files:"); 
+        if (err) {
+            console.error(err);
+        } else {
+            files.forEach((file) => { 
+                if (file.isFile()) {
+                    const fileName = path.parse(file.name).name;
+                    const fileExtension = path.extname(file.name).slice(1);
+                    const filePath = path.join(folderPath, file.name);
+                    fs.stat(filePath, (err, stats) => {
+                        if (err) {
+                            console.error(err);
+                        }
+                
+                        const fileSize = stats.size;
+                
+                        console.log(`${fileName} - ${fileExtension} - ${fileSize} bytes`);
+                    });
+                }
+            });
+        };
+    });
